5b9f2307f97deca083856164c56877931abfa37a
Sten Linnarsson <sten.linnarsson@ki.se>
2021-12-20 08:45:53 +0100

    fix unicode issue

--- shoji/workspace.py
+++ shoji/workspace.py
@@ -475,7 +475,9 @@ class WorkspaceManager:
 			group.attrs["Tensor$" + tname] = encode(pickle.dumps(tensor, protocol=4), "base-64")
 			data: np.ndarray = self[tname][:]
 			if tensor.dtype == "string":
-				data = data.astype("object")
-			group.create_dataset(tname, data=data, compression="gzip" if tensor.rank > 0 else None)
+				data = data.astype(object)
+				group.create_dataset(tname, data=data, dtype=h5py.special_dtype(vlen=str), compression="gzip" if tensor.rank > 0 else None)
+			else:
+				group.create_dataset(tname, data=data, compression="gzip" if tensor.rank > 0 else None)
 
 		h5.close()
